155e448788ea08f23f92d529be1a3fa2f74cdf2b
mashkioya <benjamin@koine.gr>
2020-10-23 22:38:41 -0500

    filled out nah_entries migration

--- database/migrations/2020_10_22_223236_create_nah_entries_table.php
+++ database/migrations/2020_10_22_223236_create_nah_entries_table.php
@@ -15,6 +15,20 @@ class CreateNahEntriesTable extends Migration
     {
         Schema::create('nah_entries', function (Blueprint $table) {
             $table->id();
+            $table->string('dialect', 3);
+            $table->string('lemma', 20);
+            $table->string('variants', 50)->nullable();
+            $table->string('plural_abs', 20)->nullable();
+            $table->string('sing_poss', 20)->nullable();
+            $table->string('plural_poss', 20)->nullable();
+            $table->string('past', 20)->nullable();
+            $table->enum('pos', ['vb', 'nn', 'pn', 'ar', 'aj', 'av', 'pp']);
+            $table->integer('verb_class')->nullable();
+            $table->foreignId('base_entry_id')->nullable();
+            $table->text('gloss_es');
+            $table->text('gloss_en')->nullable();
+            $table->text('gloss_eo')->nullable();
+            $table->text('explanation')->nullable();
             $table->timestamps();
         });
     }
